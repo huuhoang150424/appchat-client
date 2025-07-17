1d6c8bf78be979a977b132c14f2c54cb93215899
huuhoang150424 <nguyenhoanghuu15042004@gmail.com>
2025-07-18 00:03:32 +0700

    commit

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,24 +1,23 @@
+import NotfoundScreen from "../screen/notfoundScreen";
+import Login from "../screen/loginScreen";
+import Register from "../screen/registerScreen";
+import Home from "../screen/homeScreen";
 
-import NotfoundScreen from "../screen/notfoundScreen"
-import Login from "../screen/loginScreen"
-import Register from "../screen/registerScreen"
-import Home from "../screen/homeScreen"
-
-export const publicRouter:any[]=[
-    {
-        path: "/",
-        element: <Home/>
-    },
-    {
-        path: "/register",
-        element: <Register/>
-    },
-    {
-        path: "/sign",
-        element: <Login/>
-    },
-    {
-        path: "*",
-        element: <NotfoundScreen/>
-    }
-]
+export const publicRouter: any[] = [
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/register",
+		element: <Register />,
+	},
+	{
+		path: "/sign",
+		element: <Login />,
+	},
+	{
+		path: "*",
+		element: <NotfoundScreen />,
+	},
+];
